095e02ae71cbee40f2b9bd126414b5fa402eb433
romerodevelopments <gandresrp@gmail.com>
2022-11-23 08:12:53 -0300

    tipos basicos - Ejercicio de modulo

--- bases/app.js
+++ bases/app.js
@@ -1,12 +1,35 @@
 "use strict";
 (function () {
-    var a = 10; //numbers
-    var b = 10;
-    var c; //any
-    b = 3.1416;
-    function sayHello(msg) {
-        console.log(msg);
+    // Tipos
+    var batman = 'Bruce';
+    var superman = 'Clark';
+    var existe = false;
+    // Tuplas
+    var parejaHeroes = [batman, superman];
+    var villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    var aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    var SuperheroStrength;
+    (function (SuperheroStrength) {
+        SuperheroStrength[SuperheroStrength["fuerzaAcuaman"] = 0] = "fuerzaAcuaman";
+        SuperheroStrength[SuperheroStrength["fuerzaBatman"] = 1] = "fuerzaBatman";
+        SuperheroStrength[SuperheroStrength["fuerzaFlash"] = 5] = "fuerzaFlash";
+        SuperheroStrength[SuperheroStrength["fuerzaSuperman"] = 100] = "fuerzaSuperman";
+    })(SuperheroStrength || (SuperheroStrength = {}));
+    var fuerzaFlash = SuperheroStrength.fuerzaFlash;
+    var fuerzaSuperman = SuperheroStrength.fuerzaSuperman;
+    var fuerzaBatman = SuperheroStrength.fuerzaBatman;
+    var fuerzaAcuaman = SuperheroStrength.fuerzaAcuaman;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
     }
-    sayHello("Hola mundo");
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Aserciones de Tipo
+    var poder = '100';
+    var largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
-/* TIPOS DE DATOS */
--- bases/app.ts
+++ bases/app.ts
@@ -1,16 +1,47 @@
 (() => {
-  const a: number = 10; //numbers
-  let b: number = 10;
-  let c; //any
-  
-  b = 3.1416;
-  
-  function sayHello(msg: string) {
-    console.log(msg);
-  }
-  
-  sayHello("Hola mundo");
-})()
 
-/* TIPOS DE DATOS */
+    // Tipos
+    const batman: string = 'Bruce';
+    const superman: string = 'Clark';
+  
+    const existe: boolean = false;
+  
+    // Tuplas
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano: [string, number, boolean] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
 
+    enum SuperheroStrength {
+      fuerzaAcuaman,
+      fuerzaBatman,
+      fuerzaFlash = 5,
+      fuerzaSuperman = 100
+    }
+
+    const fuerzaFlash: SuperheroStrength = SuperheroStrength.fuerzaFlash;
+    const fuerzaSuperman: SuperheroStrength = SuperheroStrength.fuerzaSuperman;
+    const fuerzaBatman: SuperheroStrength = SuperheroStrength.fuerzaBatman;
+    const fuerzaAcuaman: SuperheroStrength = SuperheroStrength.fuerzaAcuaman;
+  
+    // Retorno de funciones
+    function activar_batiseñal(): string{
+      return 'activada';
+    }
+  
+    function pedir_ayuda(): void{
+      console.log('Auxilio!!!');
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
+    console.log( largoDelPoder );
+  
+  
+  })()
+  
+  
