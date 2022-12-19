bbf4e206764afa38765c94e49fbcc59c5918e334
romerodevelopments <gandresrp@gmail.com>
2022-12-19 12:07:05 -0300

    Caracteristicas de ES6 o javascript2015 disponibles a traves de typescript - Desestructuracion de objetos

--- bases/main.js
+++ bases/main.js
@@ -1,7 +1,31 @@
 "use strict";
-(function () {
-    var nombre = 'Fernando';
-    var getName = function () {
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+(() => {
+    const avengers = {
+        nick: 'Samuel L. Jackson',
+        ironman: 'Robert Downey Jr.',
+        vision: 'Paul Bettany',
+        activo: true,
+        poder: 1500.123123
+    };
+    const printAvenger = (_a) => {
+        var { ironman } = _a, resto = __rest(_a, ["ironman"]);
+        console.log(ironman, resto);
+    };
+})();
+(() => {
+    const nombre = 'Fernando';
+    const getName = () => {
         console.log('viejo getName');
     };
     getName();
