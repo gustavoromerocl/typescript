d51069a62e5c5e1fbabe02abfcab1a36516829c2
romerodevelopments <gandresrp@gmail.com>
2022-12-19 16:08:37 -0300

    clases en typescript - forma corta de asignar propiedades

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -2,28 +2,21 @@
 
     class Avenger {
 
-        private name: string;
-        private team: string;
-        public realName?: string;
+        // private name: string;
+        // private team: string;
+        // public realName?: string;
         static avgAge: number = 35;
 
         constructor( 
-            name: string, 
-            team: string, 
-            realName?: string,
-        ) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        }
-
-
+            private name: string, 
+            private team: string, 
+            public realName?: string,
+        ) {}
 
     }
 
 
     const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log( antman )
-
 
 })()
