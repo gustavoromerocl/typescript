f14ceec987f65248674ccb225e3427ff963e401f
romerodevelopments <gandresrp@gmail.com>
2022-12-20 18:40:25 -0300

    Interfaces - interfaces en las clases

--- bases/main.js
+++ bases/main.js
@@ -15,6 +15,18 @@
     };
 })();
 (() => {
+    class Mutant {
+        constructor(age, name, realName) {
+            this.age = age;
+            this.name = name;
+            this.realName = realName;
+        }
+        mutantPower(id) {
+            return this.name + ' ' + this.realName;
+        }
+    }
+})();
+(() => {
     const client = {
         name: 'Fernando',
         age: 25,
