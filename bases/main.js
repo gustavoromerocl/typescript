39bf59ce838025aa200acf56389718c2f313331a
romerodevelopments <gandresrp@gmail.com>
2022-12-19 21:07:03 -0300

    clases en typescript - clases abstractas

--- bases/main.js
+++ bases/main.js
@@ -1,5 +1,28 @@
 "use strict";
 (() => {
+    class Mutante {
+        constructor(name, realName) {
+            this.name = name;
+            this.realName = realName;
+        }
+    }
+    class Xmen extends Mutante {
+        salvarMundo() {
+            return 'Mundo a salvo!';
+        }
+    }
+    class Villian extends Mutante {
+        conquistarMundo() {
+            return 'Mundo conquistado';
+        }
+    }
+    const wolverine = new Xmen('Wolverine', 'Logan');
+    const magneto = new Villian('Magneto', 'Magnus');
+    const printName = (character) => {
+        console.log(character.realName);
+    };
+})();
+(() => {
     class Avenger {
         constructor(name, team, realName) {
             this.name = name;
