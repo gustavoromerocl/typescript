b126d8bbc3aaa96503b4d5d80dc81a2f611b818f
romerodevelopments <gandresrp@gmail.com>
2022-12-21 13:50:40 -0300

    namespaces - tarea - resolver errores en typescript

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,13 +1,12 @@
 import { Hero } from './classes/Hero';
-import powers, { Power } from './data/powers';
+// import powers, { Power } from './data/powers';
 // import { Hero as SuperHero, Hero2 } from './classes/Hero';
 // import * as HeroClasses from './classes/Hero';
 
 console.log('Hola Mundo!¡¡¡');
 
 // const ironman = new SuperHero('Ironman', 1, 55);
-const ironman = new Hero('Ironman', 10, 55);
+const ironman = new Hero('Ironman', 1, 55);
 
 console.log( ironman );
-console.log( powers );
-
+console.log( ironman.power );
