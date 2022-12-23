65fd3f22254e5790d63b74d08f2167c5e0ca639a
romerodevelopments <gandresrp@gmail.com>
2022-12-22 21:01:07 -0300

    Usando librerias que no estan escritas en typescript - trabajar con typescript en lugar de javascript

--- express-api/index.ts
+++ express-api/index.ts
@@ -1,9 +1,9 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!'})
+  res.status(200).json({ message: 'Hello World!'});
 })
 
 app.listen(port, () => {
